4ce9ca8a6bb7d687953b9736834678dfb3d5c1e6
Sean Patenio <seanjosephpatenio@gmail.com>
2026-06-24 16:46:33 +0800

    FAHHH

--- WARDS/frontend/src/main.jsx
+++ WARDS/frontend/src/main.jsx
@@ -37,6 +37,7 @@ axios.interceptors.response.use(
   }
 )
 
+console.log('[WARDS] deploy v2024.06.24')
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
